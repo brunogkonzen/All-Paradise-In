9ca2f43b994f54503c319662d43ab6b10ea17593
brunogkonzen <brunogkonzen@hotmail.com>
2024-07-04 12:46:29 -0300

    ok

--- preload.js
+++
@@ -1,5 +0,0 @@
-const { contextBridge, ipcRenderer } = require('electron');
-
-contextBridge.exposeInMainWorld('electronAPI', {
-    getUsersByScore: (type) => ipcRenderer.invoke('get-users-by-score', type)
-});
